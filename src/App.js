306bba84846252481c145b4d7e0dc8ab76d8b153
mcapurri <m.capurri@gmail.com>
2021-02-25 12:25:53 +0100

    checkbox under construction

--- src/App.js
+++ src/App.js
@@ -4,36 +4,42 @@ import usersJson from './users';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-    const [searchField, setSearchField] = useState('');
     const [users, setUsers] = useState(usersJson);
+    const [searchField, setSearchField] = useState('');
     const [isTeacher, setIsTeacher] = useState(true);
     const [isStudent, setIsStudent] = useState(true);
+    const [campus, setCampus] = useState([
+        ...new Set(users.map((user) => user.campus)),
+    ]);
 
     const handleChange = (event) => {
-        setSearchField(event.target.value);
-    };
-
-    const handleCheck = (event) => {
-        if (event.target.name === 'teacher') {
-            setIsTeacher(() => !isTeacher);
+        console.log('type', event.target.type);
+        if (event.target.type === 'checkbox') {
+            if (event.target.name === 'teacher') {
+                setIsTeacher(() => !isTeacher);
+            } else {
+                setIsStudent(() => !isStudent);
+            }
+        } else if (event.target.type === 'select') {
+            setCampus(event.target.value);
         } else {
-            setIsStudent(() => !isStudent);
+            setSearchField(event.target.value);
         }
     };
 
-    //  {isTeacher && person.role === 'teacher'}
-    // {isStudent && person.role === 'student'}
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        const filteredSearch = users.filter((person) => {
+
+        const filteredSearch = users.filter((user) => {
             return (
-                person.firstName
+                ((isTeacher || isStudent) &&
+                    `${user.firstName}`
+                        .toLowerCase()
+                        .includes(`${searchField.toLowerCase()}`)) ||
+                (`${user.lastName}`
                     .toLowerCase()
-                    .includes(searchField.toLowerCase()) ||
-                person.lastName
-                    .toLowerCase()
-                    .includes(searchField.toLowerCase())
+                    .includes(`${searchField.toLowerCase()}`) &&
+                    (user.campus === campus || !campus))
             );
         });
 
@@ -41,6 +47,17 @@ function App() {
         setUsers(() => [...filteredSearch]);
         // setSearchField('');
     };
+
+    const campusList = () => {
+        campus.map((camp) => {
+            return (
+                <option value={camp} key={camp}>
+                    {camp}
+                </option>
+            );
+        });
+    };
+
     const displayUsers = users.map((user) => {
         return (
             <tr key={uuidv4()}>
@@ -59,34 +76,68 @@ function App() {
                 style={{
                     width: '80vw',
                     display: 'flex',
+                    flexDirection: 'column',
                     justifyContent: 'space-around',
-                    margin: '5% 0 3% 5%',
+                    alignItems: 'flex-start',
                 }}
             >
-                <input
-                    type="text"
-                    name="search"
-                    id="search"
-                    placeholder="Search..."
-                    onChange={handleChange}
-                    style={{ width: '27rem' }}
-                />
-                <button type="submit">Search</button>
+                <div
+                    style={{
+                        margin: '5% 0 3% 0',
+                    }}
+                >
+                    <input
+                        type="text"
+                        name="search"
+                        id="search"
+                        placeholder="Search..."
+                        onChange={handleChange}
+                        style={{ width: '20rem' }}
+                    />
+                    <button type="submit">Search</button>
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'flex-start',
+                        width: '30rem',
+                        marginBottom: '10%',
+                    }}
+                >
+                    <div style={{ marginRight: '2%' }}>
+                        <label htmlFor="teacher">Teacher</label>
+                        <input
+                            type="checkbox"
+                            name="teacher"
+                            checked={isTeacher}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div style={{ marginRight: '2%' }}>
+                        <label htmlFor="student">Student</label>
+                        <input
+                            type="checkbox"
+                            name="student"
+                            checked={isStudent}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="campus">Campus</label>
+                        <select
+                            name="campus"
+                            id="campus"
+                            value={campus}
+                            style={{ width: ' 6rem' }}
+                            onChange={handleChange}
+                        >
+                            <option value="">All</option>
+                            {campusList}
+                        </select>
+                    </div>
+                </div>
             </form>
-            <label htmlFor="teacher">Teacher</label>
-            <input
-                type="checkbox"
-                name="teacher"
-                checked={isTeacher}
-                onChange={handleCheck}
-            />
-            <label htmlFor="student">Student</label>
-            <input
-                type="checkbox"
-                name="student"
-                checked={isStudent}
-                onChange={handleCheck}
-            />
+
             <table style={{ margin: '0 0 10% 5%' }}>
                 <thead>
                     <tr>
